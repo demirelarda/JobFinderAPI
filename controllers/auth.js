8a734b20e456eecb579e20551a5ffa69307753f3
Arda Demirel <yademirel21@gmail.com>
2023-08-25 19:02:37 +0300

    Added login controller & checking if the email & user exists

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,7 @@
 const User = require('../models/User')
 const {StatusCodes} = require('http-status-codes')
+const {BadRequestError} = require('../errors')
+const {UnauthenticatedError} = require('../errors')
 
 
 
@@ -10,7 +12,19 @@ const register = async(req,res) =>{
 }
 
 const login = async(req,res) =>{
-    res.send('login')
+    const {email, password} = req.body
+    if(!email || !password){
+        throw new BadRequestError('Please provide email and password')
+    }
+
+    const user = await User.findOne({email})
+    if(!user){
+        throw new UnauthenticatedError('Invalid Credentials')
+    }
+
+    const token = user.createJWT();
+    res.status(StatusCodes.OK).json({user:{name:user.name, id:user._id},token})
+
 }
 
 module.exports = {register,login}
